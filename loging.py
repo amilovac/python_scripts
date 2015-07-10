14c7515af28d8e297e282330392eb3a8c82150ff
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2015-07-10 15:11:51 +0200

    log kod

---
+++ loging.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info('start logging')
+logger.info('Reading...')
+logger.info('Done')
+
