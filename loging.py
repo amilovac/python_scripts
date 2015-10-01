1c19faa7041dd7deb764a4e67807c5070631d302
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2015-10-01 09:52:06 +0200

    logging slightly changed

--- loging.py
+++ loging.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
 import logging
+
+#basic configuration and instancing
 LOG_FILENAME = 'logfajl.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 logger.info('start logging')
 logger.info('Reading...')
