4f5ef02573da7171d09133928ce70bd4a8961b76
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2015-10-09 09:41:07 +0200

    addedd new files

---
+++ piblink.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# blinks one led connected to pi GPIO pin
+import RPi.GPIO as GPIO
+from time import sleep
+     
+#define the pin and its function
+LED_PIN = 25
+print "Setting up GPIO"
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+#blink function     
+def enable_led(should_enable):
+  	if should_enable:
+   		GPIO.output(LED_PIN, True)
+   	else:
+   		GPIO.output(LED_PIN, False)
+
+#start blinking     
+enable_led(False)
+print "LED is OFF"
+sleep(2)
+enable_led(True)
+print "LED is ON"
+sleep(2)
+enable_led(False)
+print "LED is OFF"
+sleep(2)
+enable_led(True)
+print "LED is ON"
+sleep(2)
+GPIO.cleanup()
---
+++ web.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#Test to see if we have working internet connection 
+import socket
+REMOTE_SERVER = "www.google.com"
+
+# check connection function
+# returns True if site is reachable and name can be resolved
+def is_connected():
+  try:
+    # see if we can resolve the host name -- tells us if there is
+    # a DNS listening
+    host = socket.gethostbyname(REMOTE_SERVER)
+    # connect to the host -- tells us if the host is actually
+    # reachable
+    s = socket.create_connection((host, 80), 2)
+    return True
+  except:
+     pass
+  return False
+print 'Internet connected? %s' %(is_connected())
+
+sleep(5)
