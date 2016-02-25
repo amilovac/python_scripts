c5361171071bcca315dfeee8e35d02b8886b1d6a
Aleksandar Milovac <aleksandar.milovac@gmail.com>
2016-02-25 13:48:06 +0100

    findafile.py

--- findafile.py
+++ findafile.py
@@ -2,7 +2,7 @@
 import os
 # find and remove file in directory
 
-for (root,files,subdirs) in os.walk('/home/pi/vezbanja'):
+for (root,files,subdirs) in os.walk('/home/pi/scripts'):
     for file in files:
         if 'hooks' in file: 
             print "I have found the: %s in: %s!" % (file, root) 
